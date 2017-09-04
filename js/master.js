29d343ce3913ceb4f8aeef8f8ccb33f2f87a8cfa
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-09-03 18:58:18 -0700

    first commit

---
+++ js/master.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('#discover-sjsu').hover(function() {
+        $('.discover-sjsu-dropdown').show();
+    }, function() {
+        $('.discover-sjsu-dropdown').hide();
+    });
+    $('#future-students').hover(function() {
+        $('.future-students-dropdown').show();
+    }, function() {
+        $('.future-students-dropdown').hide();
+    });
+    $('#curent-students').hover(function(){
+        $('.curent-students-dropdown').show();
+    },function(){
+        $('.curent-students-dropdown').hide();
+    });
+    $('#faculty-and-staff').hover(function(){
+        $('.faculty-and-staff-dropdown').show();
+    },function(){
+        $('.faculty-and-staff-dropdown').hide();
+    });
+    $('#alumni-and-community').hover(function(){
+        $('.alumni-and-community-dropdown').show();
+    },function(){
+        $('.alumni-and-community-dropdown').hide();
+    });
+});
